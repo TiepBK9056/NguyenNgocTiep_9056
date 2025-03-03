597d89d26bed34b835371a62a1bdbc4bd66dfb43
NguyenNgocTiep <nguyenngoctieptn@gmail.com>
2025-03-03 20:06:43 +0700

    Ex02: ex02_03

---
+++ lab-01/ex02/ex02_03.py
@@ -0,0 +1,6 @@
+so = int(input("Nhập một số nguyên: "))
+# Kiểm tra xem số đó có phải số chẵn hay không
+if so % 2 == 0:
+    print(so, "là số chẵn.")
+else:
+    print(so, "Không phải là số chẵn.")
