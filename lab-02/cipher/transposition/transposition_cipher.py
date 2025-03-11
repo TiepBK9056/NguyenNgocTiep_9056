d2c37edfe70319a07d7f593c405c9686268f647f
NguyenNgocTiep <nguyenngoctieptn@gmail.com>
2025-03-11 15:50:53 +0700

    [add] lab-02 web caesar

--- lab-02/cipher/transposition/transposition_cipher.py
+++ lab-02/cipher/transposition/transposition_cipher.py
@@ -11,13 +11,30 @@ class TranspositionCipher:
                 pointer += key
         return encrypted_text
     
+    # def decrypt(self, text, key):
+    #     decrypted_text = [''] * key
+    #     row, col = 0, 0
+    #     for symbol in text:
+    #         decrypted_text[col] += symbol
+    #         col += 1
+    #         if col == key or (col == key -1 and row >= len(text) % key):
+    #             col = 0
+    #             row += 1
+    #     return ''.join(decrypted_text)
+
     def decrypt(self, text, key):
-        decrypted_text = [''] * key
-        row, col = 0, 0
-        for symbol in text:
-            decrypted_text[col] += symbol
-            col += 1
-            if col == key or (col == key -1 and row >= len(text) % key):
-                col = 0
-                row += 1
+        num_rows = (len(text) + key - 1) // key  # Tính số hàng cần thiết
+        decrypted_text = [''] * num_rows  # Danh sách hàng thay vì cột
+        index = 0
+
+        for col in range(key):
+            for row in range(num_rows):
+                if index < len(text):
+                    decrypted_text[row] += text[index]
+                    index += 1
+
         return ''.join(decrypted_text)
+
+    
+
+    
